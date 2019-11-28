f74ac07a8d15dd2d425795daa9446d1ecaf40fc6
Victor Nomwesigwa <bethel@Bethel.local>
2019-11-28 14:20:56 +0300

    Login component

--- client/login.js
+++ client/login.js
@@ -1,18 +1,47 @@
 import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap-button-loader";
+
+import "./login.css";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  // Similar to componentDidMount and componentDidUpdate:
-  useEffect(() => {
-    // Update the document title using the browser API
-    document.title = `You clicked ${count} times`;
-  });
-
   return (
-    <div id="root">
-      <p>You clicked {count} times </p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+    <div className="container" id="root">
+      <div className="row">
+        <div className="col-lg-3 col-md-2" />
+        <div className="col-lg-6 col-md-8 login-box">
+          <div className="col-lg-12 login-title">Sign In</div>
+
+          <div className="col-lg-12 login-form">
+            <form onSubmit="">
+              <div className="form-group">
+                <label className="form-control-label">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  onChange=""
+                  name="email"
+                />
+              </div>
+              <div className="form-group">
+                <label className="form-control-label">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  onChange=""
+                  name="password"
+                />
+              </div>
+
+              <div className="col-lg-12 login-button center-block">
+                <Button type="submit" className="btn btn-primary" loading="">
+                  LOGIN
+                </Button>
+              </div>
+            </form>
+          </div>
+          <div className="col-lg-3 col-md-2" />
+        </div>
+      </div>
     </div>
   );
 };
